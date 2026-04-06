0e972c311c627bffdb7f59c3c5cc06cb40d07754
Andrew Roddy <andrewroddy7@gmail.com>
2026-04-06 11:16:29 -0400

    Now selects the correct database if not already created

--- config.py
+++ config.py
@@ -10,13 +10,18 @@ dbserver = pymysql.connect(
 # This creates and selects the database if it does not exist
 cursor = dbserver.cursor()
 # sql = "IF NOT EXISTS CREATE DATABASE waterfall; USE waterfall;"
-sql = "CREATE DATABASE IF NOT EXISTS waterfall;"
+sql = "CREATE DATABASE IF NOT EXISTS waterfall"
+cursor.execute(sql)
+sql = "USE waterfall"
 cursor.execute(sql)
 cursor.close()
 
-# dbserver = pymysql.connect(
-#     host="localhost",
-#     user="root",
-#    password="",
-#    database="waterfall"
-#)
+dbserver = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="waterfall"
+)
+
+# Prints the selected database
+print(f"Selected database: {dbserver.db}")
