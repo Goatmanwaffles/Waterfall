0a25e97735754090f81d8d64847a2254d761adb6
CalebStanberry <calebstanberry@gmail.com>
2026-04-07 11:34:48 -0400

    CRUD operations for students

--- sql/queries.sql
+++ sql/queries.sql
@@ -13,7 +13,6 @@ IN salary NUMERIC (8,2)
 BEGIN
 INSERT INTO INSTRUCTOR
 VALUES (id, instructor_name, department_name, salary);
-WHERE dept_name = department_name;
 END //
 DELIMITER ;
 
@@ -49,6 +48,50 @@ END //
 DELIMITER ;
 
 --CRUD Student
+
+DELIMITER //
+CREATE PROCEDURE create_student(
+IN id varchar(5),
+IN student_name varchar(20),
+IN department_name VARCHAR(20),
+IN total_credits NUMERIC (3,0)
+)
+BEGIN
+INSERT INTO STUDENT
+VALUES (id, student_name, department_name, total_credits);
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE get_students()
+BEGIN
+SELECT * FROM STUDENT;
+END //
+DELIMITER ;
+
+CREATE PROCEDURE update_student(
+IN student_id varchar(5),
+IN student_name varchar(20),
+IN department_name VARCHAR(20),
+IN total_credits NUMERIC (3,0)
+)
+BEGIN
+UPDATE STUDENT
+SET name = student_name,
+    dept_name = department_name, 
+    tot_cred = total_credits
+WHERE id = student_id;
+END //
+DELIMITER ;
+
+CREATE PROCEDURE delete_student(
+IN student_id varchar(5)
+)
+BEGIN
+DELETE FROM STUDENT
+WHERE  id = student_id;
+END //
+DELIMITER ;
 
 --CRUDT Section
 
