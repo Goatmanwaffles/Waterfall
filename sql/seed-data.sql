f621414226946bb3702cd30e9c17f4cf90ea050b
Andrew Roddy <andrewroddy7@gmail.com>
2026-04-07 10:25:29 -0400

    Reformatting Seed Data

--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -1,32 +1,43 @@
--- I want to move everything towards this format
-INSERT INTO classroom
-    VALUES ('Packard', '101', 500);
+-- Puts example data
 
-INSERT INTO classroom VALUES ('Packard', '101', '500');
-INSERT INTO classroom VALUES ('Painter', '514', '10');
-INSERT INTO classroom VALUES ('Taylor', '3128', '70');
-INSERT INTO classroom VALUES ('Watson', '100', '30');
-INSERT INTO classroom VALUES ('Watson', '120', '50');
-INSERT INTO department VALUES ('Biology', 'Watson', '90000');
-INSERT INTO department VALUES ('Comp. Sci.', 'Taylor', '100000');
-INSERT INTO department VALUES ('Elec. Eng.', 'Taylor', '85000');
-INSERT INTO department VALUES ('Finance', 'Painter', '120000');
-INSERT INTO department VALUES ('History', 'Painter', '50000');
-INSERT INTO department VALUES ('Music', 'Packard', '80000');
-INSERT INTO department VALUES ('Physics', 'Watson', '70000');
-INSERT INTO course VALUES ('BIO-101', 'Intro. to Biology', 'Biology', '4');
-INSERT INTO course VALUES ('BIO-301', 'Genetics', 'Biology', '4');
-INSERT INTO course VALUES ('BIO-399', 'Computational Biology', 'Biology', '3');
-INSERT INTO course VALUES ('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', '4');
-INSERT INTO course VALUES ('CS-190', 'Game Design', 'Comp. Sci.', '4');
-INSERT INTO course VALUES ('CS-315', 'Robotics', 'Comp. Sci.', '3');
-INSERT INTO course VALUES ('CS-319', 'Image Processing', 'Comp. Sci.', '3');
-INSERT INTO course VALUES ('CS-347', 'Database System Concepts', 'Comp. Sci.', '3');
-INSERT INTO course VALUES ('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', '3');
-INSERT INTO course VALUES ('FIN-201', 'Investment Banking', 'Finance', '3');
-INSERT INTO course VALUES ('HIS-351', 'World History', 'History', '3');
-INSERT INTO course VALUES ('MU-199', 'Music Video Production', 'Music', '3');
-INSERT INTO course VALUES ('PHY-101', 'Physical Principles', 'Physics', '4');
+-- Classroom
+INSERT INTO classroom 
+VALUES
+    ('Packard', '101', 500),
+    ('Packard', '101', '500'),
+    ('Painter', '514', '10'),
+    ('Taylor', '3128', '70'),
+    ('Watson', '100', '30'),
+    ('Watson', '120', '50');
+
+-- Department
+INSERT INTO department 
+VALUES 
+    ('Biology', 'Watson', '90000'), 
+    ('Comp. Sci.', 'Taylor', '100000'), 
+    ('Elec. Eng.', 'Taylor', '85000'), 
+    ('Finance', 'Painter', '120000'), 
+    ('History', 'Painter', '50000'), 
+    ('Music', 'Packard', '80000'), 
+    ('Physics', 'Watson', '70000');
+
+-- Course
+INSERT INTO course 
+VALUES 
+    ('BIO-101', 'Intro. to Biology', 'Biology', '4'), 
+    ('BIO-301', 'Genetics', 'Biology', '4'), 
+    ('BIO-399', 'Computational Biology', 'Biology', '3'), 
+    ('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', '4'), 
+    ('CS-190', 'Game Design', 'Comp. Sci.', '4'), 
+    ('CS-315', 'Robotics', 'Comp. Sci.', '3'), 
+    ('CS-319', 'Image Processing', 'Comp. Sci.', '3'), 
+    ('CS-347', 'Database System Concepts', 'Comp. Sci.', '3'), 
+    ('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', '3'), 
+    ('FIN-201', 'Investment Banking', 'Finance', '3'), 
+    ('HIS-351', 'World History', 'History', '3'), 
+    ('MU-199', 'Music Video Production', 'Music', '3'), 
+    ('PHY-101', 'Physical Principles', 'Physics', '4');
+
 INSERT INTO instructor VALUES ('10101', 'Srinivasan', 'Comp. Sci.', '65000');
 INSERT INTO instructor VALUES ('12121', 'Wu', 'Finance', '90000');
 INSERT INTO instructor VALUES ('15151', 'Mozart', 'Music', '40000');
