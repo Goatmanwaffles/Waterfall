022798893b30ff7d9ca91b041d2784952e59c5be
CalebStanberry <calebstanberry@gmail.com>
2026-04-07 11:28:10 -0400

    CRUD operations for instructor

--- sql/queries.sql
+++ sql/queries.sql
@@ -3,6 +3,51 @@
 
 --CRUD Instructor
 
+DELIMITER //
+CREATE PROCEDURE create_instructor(
+IN id varchar(5),
+IN instructor_name varchar(20),
+IN department_name VARCHAR(20),
+IN salary NUMERIC (8,2)
+)
+BEGIN
+INSERT INTO INSTRUCTOR
+VALUES (id, instructor_name, department_name, salary);
+WHERE dept_name = department_name;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE get_instructors()
+BEGIN
+SELECT * FROM INSTRUCTOR;
+END //
+DELIMITER ;
+
+CREATE PROCEDURE update_instructor(
+IN instructor_id varchar(5),
+IN instructor_name varchar(20),
+IN department_name VARCHAR(20),
+IN instructor_salary NUMERIC (8,2)
+)
+BEGIN
+UPDATE INSTRUCTOR
+SET name = instructor_name,
+    dept_name = department_name, 
+    salary = instructor_salary
+WHERE id = instructor_id;
+END //
+DELIMITER ;
+
+CREATE PROCEDURE delete_instructor(
+IN instructor_id varchar(5)
+)
+BEGIN
+DELETE FROM INSTRUCTOR
+WHERE  id = instructor_id;
+END //
+DELIMITER ;
+
 --CRUD Student
 
 --CRUDT Section
