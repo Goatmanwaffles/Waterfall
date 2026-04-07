060e3770d5f3710f3dd6447620b1f7ac038dc4dd
CalebStanberry <calebstanberry@gmail.com>
2026-04-07 10:55:11 -0400

    Added queries.sql

---
+++ sql/queries.sql
@@ -0,0 +1,12 @@
+
+-- CRUD operations for all major tables (Stored procedures)
+
+--CRUD Instructor
+
+--CRUD Student
+
+--CRUDT Section
+
+
+-- Major transactions e.g. enroll in class, assign instructor to course, drop a section, give a grade to section
+
