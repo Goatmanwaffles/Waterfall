8463717aeffee3977a594ceb09d686bb667c9a99
Andrew Roddy <andrewroddy7@gmail.com>
2026-04-07 11:03:01 -0400

    Fixed inserts for now

--- sql/seed-data.sql
+++ sql/seed-data.sql
@@ -1,153 +1,138 @@
--- Puts example data
-
--- Classroom
-INSERT INTO classroom 
-VALUES
-    ('Packard', '101', 500),
-    ('Packard', '101', '500'),
-    ('Painter', '514', '10'),
-    ('Taylor', '3128', '70'),
-    ('Watson', '100', '30'),
-    ('Watson', '120', '50');
-
--- Department
-INSERT INTO department 
-VALUES 
-    ('Biology', 'Watson', '90000'), 
-    ('Comp. Sci.', 'Taylor', '100000'), 
-    ('Elec. Eng.', 'Taylor', '85000'), 
-    ('Finance', 'Painter', '120000'), 
-    ('History', 'Painter', '50000'), 
-    ('Music', 'Packard', '80000'), 
-    ('Physics', 'Watson', '70000');
-
--- Course
-INSERT INTO course 
-VALUES 
-    ('BIO-101', 'Intro. to Biology', 'Biology', '4'), 
-    ('BIO-301', 'Genetics', 'Biology', '4'), 
-    ('BIO-399', 'Computational Biology', 'Biology', '3'), 
-    ('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', '4'), 
-    ('CS-190', 'Game Design', 'Comp. Sci.', '4'), 
-    ('CS-315', 'Robotics', 'Comp. Sci.', '3'), 
-    ('CS-319', 'Image Processing', 'Comp. Sci.', '3'), 
-    ('CS-347', 'Database System Concepts', 'Comp. Sci.', '3'), 
-    ('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', '3'), 
-    ('FIN-201', 'Investment Banking', 'Finance', '3'), 
-    ('HIS-351', 'World History', 'History', '3'), 
-    ('MU-199', 'Music Video Production', 'Music', '3'), 
-    ('PHY-101', 'Physical Principles', 'Physics', '4');
-
-INSERT INTO instructor VALUES ('10101', 'Srinivasan', 'Comp. Sci.', '65000');
-INSERT INTO instructor VALUES ('12121', 'Wu', 'Finance', '90000');
-INSERT INTO instructor VALUES ('15151', 'Mozart', 'Music', '40000');
-INSERT INTO instructor VALUES ('22222', 'Einstein', 'Physics', '95000');
-INSERT INTO instructor VALUES ('32343', 'El Said', 'History', '60000');
-INSERT INTO instructor VALUES ('33456', 'Gold', 'Physics', '87000');
-INSERT INTO instructor VALUES ('45565', 'Katz', 'Comp. Sci.', '75000');
-INSERT INTO instructor VALUES ('58583', 'Califieri', 'History', '62000');
-INSERT INTO instructor VALUES ('76543', 'Singh', 'Finance', '80000');
-INSERT INTO instructor VALUES ('76766', 'Crick', 'Biology', '72000');
-INSERT INTO instructor VALUES ('83821', 'Brandt', 'Comp. Sci.', '92000');
-INSERT INTO instructor VALUES ('98345', 'Kim', 'Elec. Eng.', '80000');
-INSERT INTO section VALUES ('BIO-101', '1', 'Summer', '2017', 'Painter', '514', 'B');
-INSERT INTO section VALUES ('BIO-301', '1', 'Summer', '2018', 'Painter', '514', 'A');
-INSERT INTO section VALUES ('CS-101', '1', 'Fall', '2017', 'Packard', '101', 'H');
-INSERT INTO section VALUES ('CS-101', '1', 'Spring', '2018', 'Packard', '101', 'F');
-INSERT INTO section VALUES ('CS-190', '1', 'Spring', '2017', 'Taylor', '3128', 'E');
-INSERT INTO section VALUES ('CS-190', '2', 'Spring', '2017', 'Taylor', '3128', 'A');
-INSERT INTO section VALUES ('CS-315', '1', 'Spring', '2018', 'Watson', '120', 'D');
-INSERT INTO section VALUES ('CS-319', '1', 'Spring', '2018', 'Watson', '100', 'B');
-INSERT INTO section VALUES ('CS-319', '2', 'Spring', '2018', 'Taylor', '3128', 'C');
-INSERT INTO section VALUES ('CS-347', '1', 'Fall', '2017', 'Taylor', '3128', 'A');
-INSERT INTO section VALUES ('EE-181', '1', 'Spring', '2017', 'Taylor', '3128', 'C');
-INSERT INTO section VALUES ('FIN-201', '1', 'Spring', '2018', 'Packard', '101', 'B');
-INSERT INTO section VALUES ('HIS-351', '1', 'Spring', '2018', 'Painter', '514', 'C');
-INSERT INTO section VALUES ('MU-199', '1', 'Spring', '2018', 'Packard', '101', 'D');
-INSERT INTO section VALUES ('PHY-101', '1', 'Fall', '2017', 'Watson', '100', 'A');
-INSERT INTO teaches VALUES ('10101', 'CS-101', '1', 'Fall', '2017');
-INSERT INTO teaches VALUES ('10101', 'CS-315', '1', 'Spring', '2018');
-INSERT INTO teaches VALUES ('10101', 'CS-347', '1', 'Fall', '2017');
-INSERT INTO teaches VALUES ('12121', 'FIN-201', '1', 'Spring', '2018');
-INSERT INTO teaches VALUES ('15151', 'MU-199', '1', 'Spring', '2018');
-INSERT INTO teaches VALUES ('22222', 'PHY-101', '1', 'Fall', '2017');
-INSERT INTO teaches VALUES ('32343', 'HIS-351', '1', 'Spring', '2018');
-INSERT INTO teaches VALUES ('45565', 'CS-101', '1', 'Spring', '2018');
-INSERT INTO teaches VALUES ('45565', 'CS-319', '1', 'Spring', '2018');
-INSERT INTO teaches VALUES ('76766', 'BIO-101', '1', 'Summer', '2017');
-INSERT INTO teaches VALUES ('76766', 'BIO-301', '1', 'Summer', '2018');
-INSERT INTO teaches VALUES ('83821', 'CS-190', '1', 'Spring', '2017');
-INSERT INTO teaches VALUES ('83821', 'CS-190', '2', 'Spring', '2017');
-INSERT INTO teaches VALUES ('83821', 'CS-319', '2', 'Spring', '2018');
-INSERT INTO teaches VALUES ('98345', 'EE-181', '1', 'Spring', '2017');
-INSERT INTO student VALUES ('00128', 'Zhang', 'Comp. Sci.', '102');
-INSERT INTO student VALUES ('12345', 'Shankar', 'Comp. Sci.', '32');
-INSERT INTO student VALUES ('19991', 'Brandt', 'History', '80');
-INSERT INTO student VALUES ('23121', 'Chavez', 'Finance', '110');
-INSERT INTO student VALUES ('44553', 'Peltier', 'Physics', '56');
-INSERT INTO student VALUES ('45678', 'Levy', 'Physics', '46');
-INSERT INTO student VALUES ('54321', 'Williams', 'Comp. Sci.', '54');
-INSERT INTO student VALUES ('55739', 'Sanchez', 'Music', '38');
-INSERT INTO student VALUES ('70557', 'Snow', 'Physics', '0');
-INSERT INTO student VALUES ('76543', 'Brown', 'Comp. Sci.', '58');
-INSERT INTO student VALUES ('76653', 'Aoi', 'Elec. Eng.', '60');
-INSERT INTO student VALUES ('98765', 'Bourikas', 'Elec. Eng.', '98');
-INSERT INTO student VALUES ('98988', 'Tanaka', 'Biology', '120');
-INSERT INTO takes VALUES ('00128', 'CS-101', '1', 'Fall', '2017', 'A');
-INSERT INTO takes VALUES ('00128', 'CS-347', '1', 'Fall', '2017', 'A-');
-INSERT INTO takes VALUES ('12345', 'CS-101', '1', 'Fall', '2017', 'C');
-INSERT INTO takes VALUES ('12345', 'CS-190', '2', 'Spring', '2017', 'A');
-INSERT INTO takes VALUES ('12345', 'CS-315', '1', 'Spring', '2018', 'A');
-INSERT INTO takes VALUES ('12345', 'CS-347', '1', 'Fall', '2017', 'A');
-INSERT INTO takes VALUES ('19991', 'HIS-351', '1', 'Spring', '2018', 'B');
-INSERT INTO takes VALUES ('23121', 'FIN-201', '1', 'Spring', '2018', 'C+');
-INSERT INTO takes VALUES ('44553', 'PHY-101', '1', 'Fall', '2017', 'B-');
-INSERT INTO takes VALUES ('45678', 'CS-101', '1', 'Fall', '2017', 'F');
-INSERT INTO takes VALUES ('45678', 'CS-101', '1', 'Spring', '2018', 'B+');
-INSERT INTO takes VALUES ('45678', 'CS-319', '1', 'Spring', '2018', 'B');
-INSERT INTO takes VALUES ('54321', 'CS-101', '1', 'Fall', '2017', 'A-');
-INSERT INTO takes VALUES ('54321', 'CS-190', '2', 'Spring', '2017', 'B+');
-INSERT INTO takes VALUES ('55739', 'MU-199', '1', 'Spring', '2018', 'A-');
-INSERT INTO takes VALUES ('76543', 'CS-101', '1', 'Fall', '2017', 'A');
-INSERT INTO takes VALUES ('76543', 'CS-319', '2', 'Spring', '2018', 'A');
-INSERT INTO takes VALUES ('76653', 'EE-181', '1', 'Spring', '2017', 'C');
-INSERT INTO takes VALUES ('98765', 'CS-101', '1', 'Fall', '2017', 'C-');
-INSERT INTO takes VALUES ('98765', 'CS-315', '1', 'Spring', '2018', 'B');
-INSERT INTO takes VALUES ('98988', 'BIO-101', '1', 'Summer', '2017', 'A');
-INSERT INTO takes VALUES ('98988', 'BIO-301', '1', 'Summer', '2018', null);
-INSERT INTO advisor VALUES ('00128', '45565');
-INSERT INTO advisor VALUES ('12345', '10101');
-INSERT INTO advisor VALUES ('23121', '76543');
-INSERT INTO advisor VALUES ('44553', '22222');
-INSERT INTO advisor VALUES ('45678', '22222');
-INSERT INTO advisor VALUES ('76543', '45565');
-INSERT INTO advisor VALUES ('76653', '98345');
-INSERT INTO advisor VALUES ('98765', '98345');
-INSERT INTO advisor VALUES ('98988', '76766');
-INSERT INTO time_slot VALUES ('A', 'M', '8', '0', '8', '50');
-INSERT INTO time_slot VALUES ('A', 'W', '8', '0', '8', '50');
-INSERT INTO time_slot VALUES ('A', 'F', '8', '0', '8', '50');
-INSERT INTO time_slot VALUES ('B', 'M', '9', '0', '9', '50');
-INSERT INTO time_slot VALUES ('B', 'W', '9', '0', '9', '50');
-INSERT INTO time_slot VALUES ('B', 'F', '9', '0', '9', '50');
-INSERT INTO time_slot VALUES ('C', 'M', '11', '0', '11', '50');
-INSERT INTO time_slot VALUES ('C', 'W', '11', '0', '11', '50');
-INSERT INTO time_slot VALUES ('C', 'F', '11', '0', '11', '50');
-INSERT INTO time_slot VALUES ('D', 'M', '13', '0', '13', '50');
-INSERT INTO time_slot VALUES ('D', 'W', '13', '0', '13', '50');
-INSERT INTO time_slot VALUES ('D', 'F', '13', '0', '13', '50');
-INSERT INTO time_slot VALUES ('E', 'T', '10', '30', '11', '45 ');
-INSERT INTO time_slot VALUES ('E', 'R', '10', '30', '11', '45 ');
-INSERT INTO time_slot VALUES ('F', 'T', '14', '30', '15', '45 ');
-INSERT INTO time_slot VALUES ('F', 'R', '14', '30', '15', '45 ');
-INSERT INTO time_slot VALUES ('G', 'M', '16', '0', '16', '50');
-INSERT INTO time_slot VALUES ('G', 'W', '16', '0', '16', '50');
-INSERT INTO time_slot VALUES ('G', 'F', '16', '0', '16', '50');
-INSERT INTO time_slot VALUES ('H', 'W', '10', '0', '12', '30');
-INSERT INTO prereq VALUES ('BIO-301', 'BIO-101');
-INSERT INTO prereq VALUES ('BIO-399', 'BIO-101');
-INSERT INTO prereq VALUES ('CS-190', 'CS-101');
-INSERT INTO prereq VALUES ('CS-315', 'CS-101');
-INSERT INTO prereq VALUES ('CS-319', 'CS-101');
-INSERT INTO prereq VALUES ('CS-347', 'CS-101');
-INSERT INTO prereq VALUES ('EE-181', 'PHY-101');
+insert into classroom values ('Packard', '101', '500');
+insert into classroom values ('Painter', '514', '10');
+insert into classroom values ('Taylor', '3128', '70');
+insert into classroom values ('Watson', '100', '30');
+insert into classroom values ('Watson', '120', '50');
+insert into department values ('Biology', 'Watson', '90000');
+insert into department values ('Comp. Sci.', 'Taylor', '100000');
+insert into department values ('Elec. Eng.', 'Taylor', '85000');
+insert into department values ('Finance', 'Painter', '120000');
+insert into department values ('History', 'Painter', '50000');
+insert into department values ('Music', 'Packard', '80000');
+insert into department values ('Physics', 'Watson', '70000');
+insert into course values ('BIO-101', 'Intro. to Biology', 'Biology', '4');
+insert into course values ('BIO-301', 'Genetics', 'Biology', '4');
+insert into course values ('BIO-399', 'Computational Biology', 'Biology', '3');
+insert into course values ('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', '4');
+insert into course values ('CS-190', 'Game Design', 'Comp. Sci.', '4');
+insert into course values ('CS-315', 'Robotics', 'Comp. Sci.', '3');
+insert into course values ('CS-319', 'Image Processing', 'Comp. Sci.', '3');
+insert into course values ('CS-347', 'Database System Concepts', 'Comp. Sci.', '3');
+insert into course values ('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', '3');
+insert into course values ('FIN-201', 'Investment Banking', 'Finance', '3');
+insert into course values ('HIS-351', 'World History', 'History', '3');
+insert into course values ('MU-199', 'Music Video Production', 'Music', '3');
+insert into course values ('PHY-101', 'Physical Principles', 'Physics', '4');
+insert into instructor values ('10101', 'Srinivasan', 'Comp. Sci.', '65000');
+insert into instructor values ('12121', 'Wu', 'Finance', '90000');
+insert into instructor values ('15151', 'Mozart', 'Music', '40000');
+insert into instructor values ('22222', 'Einstein', 'Physics', '95000');
+insert into instructor values ('32343', 'El Said', 'History', '60000');
+insert into instructor values ('33456', 'Gold', 'Physics', '87000');
+insert into instructor values ('45565', 'Katz', 'Comp. Sci.', '75000');
+insert into instructor values ('58583', 'Califieri', 'History', '62000');
+insert into instructor values ('76543', 'Singh', 'Finance', '80000');
+insert into instructor values ('76766', 'Crick', 'Biology', '72000');
+insert into instructor values ('83821', 'Brandt', 'Comp. Sci.', '92000');
+insert into instructor values ('98345', 'Kim', 'Elec. Eng.', '80000');
+insert into section values ('BIO-101', '1', 'Summer', '2017', 'Painter', '514', 'B');
+insert into section values ('BIO-301', '1', 'Summer', '2018', 'Painter', '514', 'A');
+insert into section values ('CS-101', '1', 'Fall', '2017', 'Packard', '101', 'H');
+insert into section values ('CS-101', '1', 'Spring', '2018', 'Packard', '101', 'F');
+insert into section values ('CS-190', '1', 'Spring', '2017', 'Taylor', '3128', 'E');
+insert into section values ('CS-190', '2', 'Spring', '2017', 'Taylor', '3128', 'A');
+insert into section values ('CS-315', '1', 'Spring', '2018', 'Watson', '120', 'D');
+insert into section values ('CS-319', '1', 'Spring', '2018', 'Watson', '100', 'B');
+insert into section values ('CS-319', '2', 'Spring', '2018', 'Taylor', '3128', 'C');
+insert into section values ('CS-347', '1', 'Fall', '2017', 'Taylor', '3128', 'A');
+insert into section values ('EE-181', '1', 'Spring', '2017', 'Taylor', '3128', 'C');
+insert into section values ('FIN-201', '1', 'Spring', '2018', 'Packard', '101', 'B');
+insert into section values ('HIS-351', '1', 'Spring', '2018', 'Painter', '514', 'C');
+insert into section values ('MU-199', '1', 'Spring', '2018', 'Packard', '101', 'D');
+insert into section values ('PHY-101', '1', 'Fall', '2017', 'Watson', '100', 'A');
+insert into teaches values ('10101', 'CS-101', '1', 'Fall', '2017');
+insert into teaches values ('10101', 'CS-315', '1', 'Spring', '2018');
+insert into teaches values ('10101', 'CS-347', '1', 'Fall', '2017');
+insert into teaches values ('12121', 'FIN-201', '1', 'Spring', '2018');
+insert into teaches values ('15151', 'MU-199', '1', 'Spring', '2018');
+insert into teaches values ('22222', 'PHY-101', '1', 'Fall', '2017');
+insert into teaches values ('32343', 'HIS-351', '1', 'Spring', '2018');
+insert into teaches values ('45565', 'CS-101', '1', 'Spring', '2018');
+insert into teaches values ('45565', 'CS-319', '1', 'Spring', '2018');
+insert into teaches values ('76766', 'BIO-101', '1', 'Summer', '2017');
+insert into teaches values ('76766', 'BIO-301', '1', 'Summer', '2018');
+insert into teaches values ('83821', 'CS-190', '1', 'Spring', '2017');
+insert into teaches values ('83821', 'CS-190', '2', 'Spring', '2017');
+insert into teaches values ('83821', 'CS-319', '2', 'Spring', '2018');
+insert into teaches values ('98345', 'EE-181', '1', 'Spring', '2017');
+insert into student values ('00128', 'Zhang', 'Comp. Sci.', '102');
+insert into student values ('12345', 'Shankar', 'Comp. Sci.', '32');
+insert into student values ('19991', 'Brandt', 'History', '80');
+insert into student values ('23121', 'Chavez', 'Finance', '110');
+insert into student values ('44553', 'Peltier', 'Physics', '56');
+insert into student values ('45678', 'Levy', 'Physics', '46');
+insert into student values ('54321', 'Williams', 'Comp. Sci.', '54');
+insert into student values ('55739', 'Sanchez', 'Music', '38');
+insert into student values ('70557', 'Snow', 'Physics', '0');
+insert into student values ('76543', 'Brown', 'Comp. Sci.', '58');
+insert into student values ('76653', 'Aoi', 'Elec. Eng.', '60');
+insert into student values ('98765', 'Bourikas', 'Elec. Eng.', '98');
+insert into student values ('98988', 'Tanaka', 'Biology', '120');
+insert into takes values ('00128', 'CS-101', '1', 'Fall', '2017', 'A');
+insert into takes values ('00128', 'CS-347', '1', 'Fall', '2017', 'A-');
+insert into takes values ('12345', 'CS-101', '1', 'Fall', '2017', 'C');
+insert into takes values ('12345', 'CS-190', '2', 'Spring', '2017', 'A');
+insert into takes values ('12345', 'CS-315', '1', 'Spring', '2018', 'A');
+insert into takes values ('12345', 'CS-347', '1', 'Fall', '2017', 'A');
+insert into takes values ('19991', 'HIS-351', '1', 'Spring', '2018', 'B');
+insert into takes values ('23121', 'FIN-201', '1', 'Spring', '2018', 'C+');
+insert into takes values ('44553', 'PHY-101', '1', 'Fall', '2017', 'B-');
+insert into takes values ('45678', 'CS-101', '1', 'Fall', '2017', 'F');
+insert into takes values ('45678', 'CS-101', '1', 'Spring', '2018', 'B+');
+insert into takes values ('45678', 'CS-319', '1', 'Spring', '2018', 'B');
+insert into takes values ('54321', 'CS-101', '1', 'Fall', '2017', 'A-');
+insert into takes values ('54321', 'CS-190', '2', 'Spring', '2017', 'B+');
+insert into takes values ('55739', 'MU-199', '1', 'Spring', '2018', 'A-');
+insert into takes values ('76543', 'CS-101', '1', 'Fall', '2017', 'A');
+insert into takes values ('76543', 'CS-319', '2', 'Spring', '2018', 'A');
+insert into takes values ('76653', 'EE-181', '1', 'Spring', '2017', 'C');
+insert into takes values ('98765', 'CS-101', '1', 'Fall', '2017', 'C-');
+insert into takes values ('98765', 'CS-315', '1', 'Spring', '2018', 'B');
+insert into takes values ('98988', 'BIO-101', '1', 'Summer', '2017', 'A');
+insert into takes values ('98988', 'BIO-301', '1', 'Summer', '2018', null);
+insert into advisor values ('00128', '45565');
+insert into advisor values ('12345', '10101');
+insert into advisor values ('23121', '76543');
+insert into advisor values ('44553', '22222');
+insert into advisor values ('45678', '22222');
+insert into advisor values ('76543', '45565');
+insert into advisor values ('76653', '98345');
+insert into advisor values ('98765', '98345');
+insert into advisor values ('98988', '76766');
+insert into time_slot values ('A', 'M', '8', '0', '8', '50');
+insert into time_slot values ('A', 'W', '8', '0', '8', '50');
+insert into time_slot values ('A', 'F', '8', '0', '8', '50');
+insert into time_slot values ('B', 'M', '9', '0', '9', '50');
+insert into time_slot values ('B', 'W', '9', '0', '9', '50');
+insert into time_slot values ('B', 'F', '9', '0', '9', '50');
+insert into time_slot values ('C', 'M', '11', '0', '11', '50');
+insert into time_slot values ('C', 'W', '11', '0', '11', '50');
+insert into time_slot values ('C', 'F', '11', '0', '11', '50');
+insert into time_slot values ('D', 'M', '13', '0', '13', '50');
+insert into time_slot values ('D', 'W', '13', '0', '13', '50');
+insert into time_slot values ('D', 'F', '13', '0', '13', '50');
+insert into time_slot values ('E', 'T', '10', '30', '11', '45 ');
+insert into time_slot values ('E', 'R', '10', '30', '11', '45 ');
+insert into time_slot values ('F', 'T', '14', '30', '15', '45 ');
+insert into time_slot values ('F', 'R', '14', '30', '15', '45 ');
+insert into time_slot values ('G', 'M', '16', '0', '16', '50');
+insert into time_slot values ('G', 'W', '16', '0', '16', '50');
+insert into time_slot values ('G', 'F', '16', '0', '16', '50');
+insert into time_slot values ('H', 'W', '10', '0', '12', '30');
+insert into prereq values ('BIO-301', 'BIO-101');
+insert into prereq values ('BIO-399', 'BIO-101');
+insert into prereq values ('CS-190', 'CS-101');
+insert into prereq values ('CS-315', 'CS-101');
+insert into prereq values ('CS-319', 'CS-101');
+insert into prereq values ('CS-347', 'CS-101');
+insert into prereq values ('EE-181', 'PHY-101');
