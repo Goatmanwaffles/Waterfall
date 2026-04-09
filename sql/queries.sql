2550735193599d33c3c504bcb86a48a6cafda551
CalebStanberry <calebstanberry@gmail.com>
2026-04-09 12:21:57 -0400

    Fixed queries

--- sql/queries.sql
+++ sql/queries.sql
@@ -212,10 +212,10 @@ END;;
 DROP PROCEDURE IF EXISTS enroll_in_section;;
 CREATE PROCEDURE enroll_in_section(
     IN temp_student_ID int,
-    IN temp_section_ID int,
+    IN temp_section_ID int
 )
 BEGIN
-    INSERT INTO TAKES(student_ID, course_ID)
+    INSERT INTO TAKES(student_ID, section_ID)
     VALUES (temp_student_ID, temp_section_ID);
 END;;
 
@@ -223,7 +223,7 @@ END;;
 DROP PROCEDURE IF EXISTS assign_instructor_to_section;;
 CREATE PROCEDURE assign_instructor_to_section(
     IN temp_instructor_ID int,
-    IN temp_section_ID int,
+    IN temp_section_ID int
 )
 BEGIN
     INSERT INTO TEACHES(instructor_ID, section_ID)
@@ -233,21 +233,11 @@ END;;
 -- Drop Section Transaction
 DROP PROCEDURE IF EXISTS drop_section_transaction;;
 CREATE PROCEDURE drop_section_transaction(
-    IN temp_section_ID int,
-    IN temp_course_ID int,
-    IN temp_semester VARCHAR(6),
-    IN temp_year NUMERIC(4,0),
-    IN temp_building_ID INT,
-    IN temp_time_slot_ID INT
+    IN temp_section_ID int
 )
 BEGIN
     DELETE FROM SECTION
-    WHERE course_ID = temp_course_ID,
-        AND section_ID = temp_section_ID,
-        AND semester = temp_semester,
-        AND year = temp_year,
-        AND building_ID = temp_building_ID,
-        AND time_slot_ID = temp_time_slot_ID,
+    WHERE section_ID = temp_section_ID;
 END;;
 
 -- Give Grade to Section
@@ -255,11 +245,11 @@ DROP PROCEDURE IF EXISTS give_grade_to_section;;
 CREATE PROCEDURE give_grade_to_section(
     IN temp_student_ID int,
     IN temp_section_ID int,
-    IN temp_grades VARCHAR(2),
+    IN temp_grades VARCHAR(2)
 )
 BEGIN
     UPDATE TAKES
-    SET grade = temp_grade
+    SET grades = temp_grades
     WHERE student_ID = temp_student_ID
-        AND section_ID = temp_section_ID
+        AND section_ID = temp_section_ID;
 END;;
