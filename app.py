84083e9d48753aa5e280c094133445b98ba698b3
Andrew Roddy <andrewroddy7@gmail.com>
2026-04-26 16:33:44 -0400

    Fixed the favicon

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ Session(app)
 
 loadBlueprints(app)
 
+@app.route('/favicon.ico')
+def favicon():
+    return app.send_static_file('favicon.ico')
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
